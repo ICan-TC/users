fe2ef94860df14ffd7b132615712082563aec39b
Mhirii <ahmedmhiri218@gmail.com>
2025-12-12 18:46:12 +0100

    fix tags

--- internal/models/students.go
+++ internal/models/students.go
@@ -12,7 +12,7 @@ type Students struct {
 	Level         *string   `bun:"level"`
 	CreatedAt     time.Time `bun:"created_at,default:current_timestamp"`
 	UpdatedAt     time.Time `bun:"updated_at,default:current_timestamp"`
-	DeletedAt     time.Time `bun:deleted_at,default:null`
+	DeletedAt     time.Time `bun:"deleted_at,default:null"`
 
 	UserID *string `bun:"user_id"`
 	User   *Users  `bun:"rel:belongs-to,join:user_id=id"`
